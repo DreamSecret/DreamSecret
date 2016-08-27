b7f142960959d6c7fb0b1bd8f2127d0dc519646c
Ray Vincent Phillip D. Villaver <phillip60341@gmail.com>
2016-08-27 21:12:35 +0800

    function type for add resolved

--- php_raw/add_resolved_php.php
+++ php_raw/add_resolved_php.php
@@ -12,4 +12,10 @@
    $query = "insert into resolved_problem($fields) values($values)";
    mysqli_query($conn,$query);
    //sample comment1
+
+   function add_resolved($photo,$photo_date,$problem_id,$account_id,$points){
+      $query = "insert into resolved_problems(photo,date_uploaded,photo_date,problem_id,account_id,points) values($photo,now(),$photo_date,$problem_id,$account_id,$points)";
+
+      mysqli_query($conn,$query);
+   }
 ?>
