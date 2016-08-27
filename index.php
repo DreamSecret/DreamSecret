36663107decc772519ca28848a760979d62b80f8
Ray Vincent Phillip D. Villaver <phillip60341@gmail.com>
2016-08-27 22:45:52 +0800

    verds daw

--- index.php
+++ index.php
@@ -6,13 +6,42 @@
 
 	<?php  include('php/style.php'); ?>
 
+		<link rel="stylesheet" type="text/css" href="css/style.css"></link>
+
+
 </head>
 
 <body>
-	<a class="btn btn-default" href="#" role="button">Link</a>
-	<button class="btn btn-default" type="submit">Button</button>
-	<input class="btn btn-default" type="button" value="Input">
-	<input class="btn btn-default" type="submit" value="Submit">
+
+	<div class="row">
+
+		<div class="col-xs-12">
+
+		</div>
+
+		<div class="col-xs-2">
+
+		</div>
+
+		<div class="col-xs-8 black-box">
+			<form>
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Ranger Name:</label>
+			    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">Password:</label>
+			    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+			  </div>
+		
+			  <button type="submit" class="btn btn-success btn-block">Login</button>
+			</form>
+	</div>
+		</div>
+
+	<div class="col-xs-2">
+
+	</div>
 
 </body>
 
---
+++ php/tracer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<?php  include('style.php'); ?>
+
+</head>
+<div class="row">
+	<div class="col-xs-4">
+		<button type="button" class="btn btn-success btn-block">FORUM</button>
+	</div>
+	<div class="col-xs-4">
+		<button type="button" class="btn btn-success btn-block">RESOVLED</button>
+	</div>
+	<div class="col-xs-4">
+		<button type="button" class="btn btn-primary btn-block">SIMULATE</button>
+	</div>
+	
+	<div class="col-xs-12">
+
+	</div>
+
+</div>
+</html>
