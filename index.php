687574b31927dcc402c76844baa69a07b30934a1
John Villete <spinnerjohnv@gmail.com>
2016-08-27 15:18:24 +0800

    Initial Setup

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-Test
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<?php  include('php/style.php'); ?>
+
+</head>
+
+<body>
+	<a class="btn btn-default" href="#" role="button">Link</a>
+	<button class="btn btn-default" type="submit">Button</button>
+	<input class="btn btn-default" type="button" value="Input">
+	<input class="btn btn-default" type="submit" value="Submit">
+
+</body>
+
+</html>
